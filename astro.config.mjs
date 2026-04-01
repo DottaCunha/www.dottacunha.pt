cf5aea548c26eedeb5d3d10c8f4a23606da5bd4a
Diogo Cunha <dfcu@MacBookPro.lan>
2026-04-02 00:28:53 +0100

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://dottacunha.pt',
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [sitemap()],
 });
